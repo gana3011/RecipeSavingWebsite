54b68138e9437267364392cf327cc84e3ef1935a
Ganapathi Subramanian <gana301104@gmail.com>
2025-06-06 18:22:53 +0530

    added debug class

--- backend/src/main/java/com/project/recipe/Configuration/MailDebugConfig.java
+++ backend/src/main/java/com/project/recipe/Configuration/MailDebugConfig.java
@@ -3,6 +3,7 @@ package com.project.recipe.Configuration;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class MailDebugConfig {
